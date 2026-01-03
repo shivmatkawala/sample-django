3eb3f7e2c9db5404659579fd306a0d9ebb90d0f4
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-01-03 12:22:35 +0530

    created sample django project

---
+++ app1/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class SignupModel(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField(unique=True, max_length=30)
+    contact_no = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
+    cnf_password = models.CharField(max_length=10)
+
+
+class SigninModel(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=10)
